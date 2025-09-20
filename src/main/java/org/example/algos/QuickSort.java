f8bfce3def2f44054b86bd38f295e608ca6892ae
MAKLUBE <semenovrasul7@gmail.com>
2025-09-20 18:18:13 +0500

    Create QuickSort.java

---
+++ src/main/java/org/example/algos/QuickSort.java
@@ -0,0 +1,10 @@
+package org.example.algos;
+
+public final class QuickSort {
+    private QuickSort() {}
+
+    // TODO: randomized pivot + рекурсия сначала на меньшую сторону
+    public static void sort(int[] a) {
+        // временно пусто
+    }
+}
