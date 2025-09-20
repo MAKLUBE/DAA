349cf2b278ff19fe1d48d5ac8b67f01c508f02b3
MAKLUBE <semenovrasul7@gmail.com>
2025-09-20 18:19:20 +0500

    Create ClosestPair2D.java

---
+++ src/main/java/org/example/algos/ClosestPair2D.java
@@ -0,0 +1,10 @@
+package org.example.algos;
+
+public final class ClosestPair2D {
+    private ClosestPair2D() {}
+
+    // TODO: divide & conquer: вернуть расстояние между ближайшей парой
+    public static double distance(double[][] pts) {
+        throw new UnsupportedOperationException("not yet");
+    }
+}
