947fdffd40018173855b6ba64403972b39ef4ce6
MAKLUBE <semenovrasul7@gmail.com>
2025-09-20 18:23:41 +0500

    Create tempTest.java

---
+++ src/test/java/org/example/tempTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class tempTest {
+    @Test void ok() {
+        assertEquals(2, 1 + 1);
+    }
+}
