afc3ccf36f20a5299cd91b49ccebc17948b4475c
MAKLUBE <semenovrasul7@gmail.com>
2025-09-20 18:18:55 +0500

    Update DeterministicSelect.java

--- src/main/java/org/example/algos/DeterministicSelect.java
+++ src/main/java/org/example/algos/DeterministicSelect.java
@@ -1,1 +1,10 @@
+package org.example.algos;
 
+public final class DeterministicSelect {
+    private DeterministicSelect() {}
+
+    // TODO: median-of-medians (MoM5). Возвращает k-й по порядку (0-based)
+    public static int selectKth(int[] a, int k) {
+        throw new UnsupportedOperationException("not yet");
+    }
+}
