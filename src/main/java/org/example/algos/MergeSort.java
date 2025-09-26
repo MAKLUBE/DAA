4d27c7d55f799fb9270ff8bf1e7015dccafdbeba
MAKLUBE <semenovrasul7@gmail.com>
2025-09-26 15:37:12 +0500

    Update MergeSort.java

--- src/main/java/org/example/algos/MergeSort.java
+++ src/main/java/org/example/algos/MergeSort.java
@@ -1,10 +1,4 @@
 package org.example.algos;
 
-public final class MergeSort {
-    private MergeSort() {}
-
-    // TODO: реализовать MergeSort с единым буфером и cutoff на insertion sort
-    public static void sort(int[] a) {
-        // временно пусто
-    }
+public class MergeSort {
 }
